13296f1806171fa0665f3206b6c29be22f174b76
sapirdeu <45518437+sapirdeu@users.noreply.github.com>
2020-05-20 19:48:41 +0300

    Add files via upload

---
+++ FlightSimulatorApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using FlightSimulatorApp.Model;
+using FlightSimulatorApp.ViewModel;
+
+namespace FlightSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public ConnectVM ConnectVM { get; internal set; }
+        public DashboardVM DashboardVM { get; internal set; }
+        public ControlBoardVM ControlBoardVM { get; internal set; }
+        public MapVM MapVM { get; internal set; }
+
+
+        // Initializing the simulator model and all the view models.
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ISimulatorModel simulatorModel = new SimulatorModel(new TelnetClient());
+            ConnectVM = new ConnectVM(simulatorModel);
+            DashboardVM = new DashboardVM(simulatorModel);
+            ControlBoardVM = new ControlBoardVM(simulatorModel);
+            MapVM = new MapVM(simulatorModel);
+        }
+    }
+}
---
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlightSimulatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Ctor.
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Binding between the view to it's suitable view model.
+            myConnect.DataContext = (Application.Current as App).ConnectVM;
+            myConnect.SetConnectVM((Application.Current as App).ConnectVM);
+            myControlBoard.DataContext = (Application.Current as App).ControlBoardVM;
+            myControlBoard.SetControlBoardVM((Application.Current as App).ControlBoardVM);
+            myDashBoard.DataContext = (Application.Current as App).DashboardVM;
+            myMap.DataContext = (Application.Current as App).MapVM;
+        }
+    }
+}
